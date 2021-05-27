ca0408185564263ac3483ecd01b81924f9ba61be
Grama Nicolae <gramanicu@gmail.com>
2021-05-27 12:31:20 +0300

    Updated scripts

--- scripts/install_redis.sh
+++ scripts/install_redis.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-if ! command -v redis-cli &> /dev/null
+if ! command -v redis-server &> /dev/null
 then
+    cd $(dirname $0)
     sudo apt update
     sudo apt install redis-server
     sudo cp redis.conf /etc/redis/redis.conf
