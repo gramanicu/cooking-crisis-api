4663f861f176a39fbf5bbf9130f7c4c4a3e780c3
Grama Nicolae <gramanicu@gmail.com>
2021-06-02 14:37:06 +0300

    Solved small account activation bug

--- configs/development.js
+++ configs/development.js
@@ -3,7 +3,8 @@
 let devConfig = {
     hostname: "https://cooking-crisis-api-dev.herokuapp.com",
     port: process.env.PORT || 3000,
-    activation_address: "https://cooking-crisis-web-dev.herokuapp.com",
+    activation_address:
+        "https://cooking-crisis-web-dev.herokuapp.com/activate/",
     verify_activated: true,
 }
 
